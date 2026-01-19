6f38d043cc40a543c6649debf4ee55a2379e4392
Anuroopa-007 <anuroopa@tansam.org>
2026-01-19 15:54:34 +0530

    ceo dashboard added

---
+++ tansam_project_management/src/dashboards/Ceo/CeoDashboard.jsx
@@ -0,0 +1,12 @@
+function CeoDashboard() {
+  return (
+    <div>
+      <h1>CEO Dashboard</h1>
+      <p>
+        Organization Overview: Revenue, Projects, Performance & Strategy
+      </p>
+    </div>
+  );
+}
+
+export default CeoDashboard;
