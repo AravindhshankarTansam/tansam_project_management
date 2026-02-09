7f0a38752f93f914255f9b5bc382c362657bf8ed
Aravindh <aravindhshankar@tansam.org>
2026-02-09 14:25:25 +0530

    db error

--- tansam_project_management/backend/src/schema/project/assignTeam/assignTeam.schema.js
+++ tansam_project_management/backend/src/schema/project/assignTeam/assignTeam.schema.js
@@ -1,12 +1,12 @@
 export const createAssignTeamSchema = async (db) => {
   await db.execute(`
     CREATE TABLE IF NOT EXISTS project_team_assignments (
-      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      id INT AUTO_INCREMENT PRIMARY KEY,
 
-      project_id INT UNSIGNED NOT NULL,
+      project_id INT NOT NULL,
       member_name VARCHAR(150) NOT NULL,
       role VARCHAR(100) NOT NULL,
-      department_id INT UNSIGNED NOT NULL,
+      department_id INT NOT NULL,
 
       start_date DATE NOT NULL,
       end_date DATE NOT NULL,
